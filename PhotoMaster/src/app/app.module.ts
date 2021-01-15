dec09d332731283308deeaa7c8e29d753e9194c8
HungerKiller <zhongshui.wang.chn@gmail.com>
2021-01-15 17:53:32 +0100

    add router link

--- PhotoMaster/src/app/app.module.ts
+++ PhotoMaster/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,19 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { MenuComponent } from './Components/menu/menu.component';
 
+import { MenuComponent } from './Components/menu/menu.component';
+import { ContactComponent } from './Components/contact/contact.component';
 import { LabelComponent } from './Module-Label/label.component';
 import { PhotoComponent } from './Module-Photo/photo.component';
-import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    ContactComponent,
     LabelComponent,
-    PhotoComponent,
-    ContactComponent
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
