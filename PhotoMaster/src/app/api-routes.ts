64425448b6100373746b387c6296fc2fc2bc07e7
HungerKiller <zhongshui.wang.chn@gmail.com>
2021-01-16 16:49:29 +0100

    add api routes

---
+++ PhotoMaster/src/app/api-routes.ts
@@ -0,0 +1,26 @@
+import { environment } from '../environments/environment';
+
+export class ApiRoute {
+    private static readonly BaseUrl = environment.baseUrl;
+
+    public static readonly LABEL = {
+        getLabels: () => `${ApiRoute.BaseUrl}/labels`,
+        getLabel: (labelId: number) => `${ApiRoute.BaseUrl}/labels/${labelId}`,
+        postLabel: () => `${ApiRoute.BaseUrl}/labels`,
+        putLabel: (labelId: number) => `${ApiRoute.BaseUrl}/labels/${labelId}`,
+        deleteLabel: (labelId: number) => `${ApiRoute.BaseUrl}/labels/${labelId}`
+    }
+
+    public static readonly PHOTO = {
+        getPhotos: () => `${ApiRoute.BaseUrl}/photos`,
+        getPhoto: (photoId: number) => `${ApiRoute.BaseUrl}/photos/${photoId}`,
+        getPhotosByLabels: (labelIds: number[]) => {
+            let params = '';
+            labelIds.map(id => params += `&ids=${id}`);
+            return `${ApiRoute.BaseUrl}/photos/bylabels?${params}`;
+        },
+        postPhoto: () => `${ApiRoute.BaseUrl}/photos`,
+        putPhoto: (photoId: number) => `${ApiRoute.BaseUrl}/photos/${photoId}`,
+        deletePhoto: (photoId: number) => `${ApiRoute.BaseUrl}/photos/${photoId}`
+    }
+}
