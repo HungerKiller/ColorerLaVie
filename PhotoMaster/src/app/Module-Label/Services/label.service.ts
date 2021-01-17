007e832e7fbdf53d67f5f26e134c75b2a1ac2750
HungerKiller <zhongshui.wang.chn@gmail.com>
2021-01-17 11:36:34 +0100

    implement label service api call

--- PhotoMaster/src/app/Module-Label/Services/label.service.ts
+++ PhotoMaster/src/app/Module-Label/Services/label.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, observable, of } from 'rxjs'
+import { ApiRoute } from 'src/app/api-routes';
 import { Label } from '../Models/Label';
 
 @Injectable({
@@ -11,6 +12,22 @@ export class LabelService {
   constructor(private http: HttpClient) { }
 
   getLabels(): Observable<Label[]> {
-    return this.http.get<Label[]>(`https://localhost:44344/api/Labels`);
+    return this.http.get<Label[]>(ApiRoute.LABEL.getLabels());
+  }
+
+  getLabel(labelId: number): Observable<Label> {
+    return this.http.get<Label>(ApiRoute.LABEL.getLabel(labelId));
+  }
+
+  postLabel(): Observable<Label> {
+    return this.http.get<Label>(ApiRoute.LABEL.postLabel()); // todo add body
+  }
+
+  putLabel(labelId: number): Observable<Label> {
+    return this.http.get<Label>(ApiRoute.LABEL.putLabel(labelId)); // todo add body
+  }
+
+  deleteLabel(labelId: number): Observable<Label> {
+    return this.http.get<Label>(ApiRoute.LABEL.deleteLabel(labelId)); // todo return value
   }
 }
