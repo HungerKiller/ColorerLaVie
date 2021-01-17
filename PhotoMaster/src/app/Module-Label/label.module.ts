76d26d9eb0149ff991558c7a7ea24db13cd7de23
HungerKiller <zhongshui.wang.chn@gmail.com>
2021-01-17 23:43:21 +0100

    implement label-list page

--- PhotoMaster/src/app/Module-Label/label.module.ts
+++ PhotoMaster/src/app/Module-Label/label.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 import { LabelComponent } from './label.component';
 import { LabelDetailComponent } from './Components/label-detail/label-detail.component';
@@ -14,8 +18,12 @@ import { LabelListComponent } from './Components/label-list/label-list.component
   imports: [
     CommonModule,
     RouterModule,
-    MatPaginatorModule,
-    MatTableModule
+    NzTableModule,
+    NzButtonModule,
+    NzDividerModule,
+    NzPaginationModule,
+    NzIconModule,
+    NzGridModule
   ]
 })
 export class LabelModule { }
